7ace3eeb76d3ae7f09ffef9ecfbba859e95a7534
karmi214 <karmi214@gmail.com>
2019-08-22 16:53:23 +0545

    changed main

--- main.tf
+++ main.tf
@@ -32,32 +32,32 @@ resource "aws_security_group" "rdp" {
   ingress {
     from_port   = 10933
     to_port     = 10933
-    protocol    = "0"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "0"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 8080
     to_port     = 8080
-    protocol    = "0"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 3389
     to_port     = 3389
-    protocol    = "0"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port       = 0
     to_port         = 0
-    protocol        = "0"
+    protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
