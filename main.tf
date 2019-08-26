bb5ba2b27b16f76b41c2d88295afca30a43837a5
karmi214 <karmi214@gmail.com>
2019-08-26 14:28:18 +0545

    test

--- main.tf
+++ main.tf
@@ -7,61 +7,75 @@ terraform {
     region = "us-east-1"
   }
 }
-data "template_file" "init" {
-  template = "${file("init.tpl")}"
-  vars = {
-    thumbprint  = "${var.thumbprint}"
-    server_url  = "${var.server_url}"
-    api_key     = "${var.api_key}"
-    role        = "${var.role}"
-    environment = "${var.environment}"
-  }
+variable "bucket_name" {
+
 }
 
-resource "aws_instance" "web" {
-  ami             = "${var.ami}"
-  instance_type   = "t2.medium"
-  user_data       = "${data.template_file.init.rendered}"
-  key_name        = "${var.keypair}"
-  security_groups = ["${aws_security_group.rdp.name}"]
+resource "aws_s3_bucket" "b" {
+  bucket = "${var.bucket_name}"
+  acl    = "private"
+
   tags = {
-    Name        = "Octopus Instance"
-    Environment = "Test"
+    Name        = "My bucket"
+    Environment = "${var.environment}"
   }
 }
-resource "aws_security_group" "rdp" {
-  name        = "Octopusgroup"
-  description = "security group for Octopus"
 
-  ingress {
-    from_port   = 10933
-    to_port     = 10933
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# data "template_file" "init" {
+#   template = "${file("init.tpl")}"
+#   vars = {
+#     thumbprint  = "${var.thumbprint}"
+#     server_url  = "${var.server_url}"
+#     api_key     = "${var.api_key}"
+#     role        = "${var.role}"
+#     environment = "${var.environment}"
+#   }
+# }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_instance" "web" {
+#   ami             = "${var.ami}"
+#   instance_type   = "t2.medium"
+#   user_data       = "${data.template_file.init.rendered}"
+#   key_name        = "${var.keypair}"
+#   security_groups = ["${aws_security_group.rdp.name}"]
+#   tags = {
+#     Name        = "Octopus Instance"
+#     Environment = "Test"
+#   }
+# }
+# resource "aws_security_group" "rdp" {
+#   name        = "Octopusgroup"
+#   description = "security group for Octopus"
+
+#   ingress {
+#     from_port   = 10933
+#     to_port     = 10933
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 3389
+#     to_port     = 3389
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
